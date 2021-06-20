2bd51aac2ada330afbff9dbda37667eaa471127e
paribartan <paribartankalathoki@gmail.com>
2021-06-20 22:38:22 +0545

    added grouping data set

--- ag-grid/src/app/app.component.ts
+++ ag-grid/src/app/app.component.ts
@@ -10,8 +10,10 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   @ViewChild('agGrid') agGrid: AgGridAngular;
-  
+
   @ViewChild('agGridForLargeDataSet') agGridForLargeDataSet: AgGridAngular;
+
+  @ViewChild('agGridForGroupedDataSet') agGridForGroupedDataSet: AgGridAngular;
 
   constructor(private http: HttpClient) { }
 
@@ -38,17 +40,41 @@ export class AppComponent implements OnInit {
   // to filtering and sorting, grouping is another effective way for the user to make sense out of large amounts of data.
   largeAmountRowDatas: Observable<any[]>;
 
+
+  // lets grouping data
+  defaultGroupingColDef = {
+    sortable: true,
+    filter: true
+  };
+
+  groupingColumnDefs = [
+    { field: 'make', rowGroup: true },
+    { field: 'price' }
+  ];
+
+  autoGroupColumnDef = {
+    headerName: 'Model',
+    field: 'model',
+    cellRenderer: 'agGroupCellRenderer',
+    cellRendererParams: {
+      checkbox: true
+    }
+  };
+
+  groupingRowDatas: Observable<any[]>;
+
   ngOnInit(): void {
     this.fetchSmallDataSet();
     this.fetchLargeAmountDataSet();
   }
 
-  fetchSmallDataSet() {
+  fetchSmallDataSet(): void {
     this.rowDatas = this.http.get<any[]>('https://www.ag-grid.com/example-assets/small-row-data.json');
   }
 
-  fetchLargeAmountDataSet() {
+  fetchLargeAmountDataSet(): void {
     this.largeAmountRowDatas = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+    this.groupingRowDatas = this.largeAmountRowDatas;
   }
 
   getSelectedRows(): void {
@@ -59,9 +85,22 @@ export class AppComponent implements OnInit {
     alert(`Selected nodes: ${selectedDataStringPresentation}`);
   }
 
-  getSelectedRowsForLargeAmountOfData() {
+  getSelectedRowsForLargeAmountOfData(): void {
     const selectedNodes = this.agGridForLargeDataSet.api.getSelectedNodes();
     const selectedData = selectedNodes.map(node => node.data);
+    const selectedDataStringPresentation = selectedData.map(node => `${node.make} ${node.model}`).join(', ');
+
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
+
+  getSelectedGroupRowsForLargeAmountOfData(): void {
+    const selectedNodes = this.agGridForGroupedDataSet.api.getSelectedNodes();
+    const selectedData = selectedNodes.map(node => {
+      if (node.groupData) {
+        return { make: node.key, model: 'Group' };
+      }
+      return node.data;
+    });
     const selectedDataStringPresentation = selectedData.map(node => `${node.make} ${node.model}`).join(', ');
 
     alert(`Selected nodes: ${selectedDataStringPresentation}`);
